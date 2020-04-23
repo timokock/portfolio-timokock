cf24a5c39ce9ea0d3dffc99f75191e685162b0fe
Timo Kock <timo.kock@movicoders.com>
2020-04-23 21:17:29 +0200

    Index Page Styling
    Implemented html on index page and css styles

--- components/layouts/BaseLayout.js
+++ components/layouts/BaseLayout.js
@@ -1,13 +1,16 @@
-import { Fragment } from 'react';
 import Header from '../shared/Header';
 
 const BaseLayout = (props) => {
+    const { className, children } = props;
     return (
-        <Fragment>
-            <Header />
-            {props.children}
-        </Fragment>
-
+        <div className="layout-container" >
+        <Header />
+        <main className={`cover ${className}`}>
+          <div className="wrapper">
+            {children}
+          </div>
+        </main>
+      </div>
     )
 };
 
