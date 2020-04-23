e20fe904689a04385280b868f9e09355fc65d4ba
Timo Kock <timo.kock@movicoders.com>
2020-04-23 19:35:22 +0200

    Bootstrap Integration
    Implemented reactstrap & bootstrap

--- components/shared/Header.js
+++ components/shared/Header.js
@@ -7,16 +7,11 @@ class Header extends Component {
     render() {
         return(
             <Fragment>
-                {this.props.children}
-                <p className="customClass">styled p element</p>
-                <p className="customClassFile">styled p element</p>
-                <Link href="/"><a>Home</a></Link>
-                <Link href="/about"><a>About</a></Link>
-                <Link href="/portfolios"><a>Portfolio</a></Link>
-                <Link href="/blogs"><a>Blog</a></Link>
-                <Link href="/cv"><a>CV</a></Link>
-                <NextLink route='/test/2'>Test 2</NextLink>
-                <NextLink route='/test/5'>Test 5</NextLink>
+                <Link href="/"><a> Home </a></Link>
+                <Link href="/about"><a> About </a></Link>
+                <Link href="/portfolios"><a> Portfolio </a></Link>
+                <Link href="/blogs"><a> Blog </a></Link>
+                <Link href="/cv"><a> CV </a></Link>
             </Fragment>
         );
     };
